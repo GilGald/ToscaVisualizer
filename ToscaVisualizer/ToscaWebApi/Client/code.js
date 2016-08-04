2c2028378ebf9f8eabc916f15177c6cafc81e93a
GilGald <gil.2006@gmail.com>
2016-08-04 11:49:40 +0300

    Removing unwanted code

--- ToscaVisualizer/ToscaWebApi/Client/code.js
+++ ToscaVisualizer/ToscaWebApi/Client/code.js
@@ -21,38 +21,11 @@ $(function(){ // on dom ready
                 contentType: false,
                 processData: false
             });
-
-
-
         });
 
 
 
     
-
-var urlOfJson="https://raw.githubusercontent.com/GilGald/ToscaVisualizer/master/Client/jsonTry.json"
-
-
-
- function loadJSON(callback) {   
-
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', urlOfJson, true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);  
- }
- 
- 
-
-
-
-
 
 var cy = cytoscape({
   container: document.getElementById('cy'),  
@@ -146,18 +119,6 @@ function createEdges(json){
 	
 	return edges;		
 }
-//
-//
-// function init() { 
-//	 json=loadJSON(function(response) {
-//	  // Parse JSON string into object
-//		var actual_JSON = JSON.parse(response);
-//		addElements(createNodes(actual_JSON),createEdges(actual_JSON));		
-//	 });
-//	 
-//}
-// 
  
-
 	
 }); // on dom ready
